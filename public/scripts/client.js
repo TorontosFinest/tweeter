4adc441390edf8302b2082b4fedf1fa57a8fc7d5
TorontosFinest <abdialicanada20@gmail.com>
2021-12-15 22:25:16 +0000

    implemented loadtweets function, and added validation for the form

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,55 +4,6 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 $(document).ready(function () {
-  const data = [
-    {
-      user: {
-        name: "Newton",
-        avatars: "https://i.imgur.com/73hZDYK.png",
-        handle: "@SirIsaac",
-      },
-      content: {
-        text: "If I have seen further it is by standing on the shoulders of giants",
-      },
-      created_at: 1461116232227,
-    },
-    {
-      user: {
-        name: "Descartes",
-        avatars: "https://i.imgur.com/nlhLi3I.png",
-        handle: "@rd",
-      },
-      content: {
-        text: "Je pense , donc je suis",
-      },
-      created_at: 1461113959088,
-    },
-  ];
-
-  //   function createTweetElement(tweetObject) {
-  //     console.log(tweetObject);
-  //     const $tweet = `<article class="tweet">
-  //     <header>
-  //       <i class="far fa-grin-squint" data-src=${tweetObject.user.avatars}>${
-  //       tweetObject.user.name
-  //     }</i>
-  //       <span>${tweetObject.user.handle}</span>
-  //     </header>
-
-  //     <p>${tweetObject.content.text}</p>
-  //     <footer>
-  //       <span>${timeago.format(tweetObject.created_at)}</span>
-  //       <div>
-  //         <i class="fas fa-flag"></i>
-  //         <i class="fas fa-retweet"></i>
-  //         <i class="fas fa-heart"></i>
-  //       </div>
-  //     </footer>
-  //   </article>;`;
-
-  //     return $tweet;
-  //   }
-
   function createTweetElement(tweetObject) {
     console.log(tweetObject);
     const $tweet = `<article class="tweet">
@@ -79,6 +30,26 @@ $(document).ready(function () {
     return $tweet;
   }
 
+  $("#tweet-form").on("submit", function (event) {
+    if ($("#tweet-text").val().length > 140) {
+      alert("TOO MANY CHARACTERS!");
+    } else if (!$("#tweet-text").val()) {
+      alert("TWEET IS EMPTY!");
+    } else {
+      event.preventDefault();
+      console.log("the form was submitted!");
+      const serialiedData = $(this).serialize();
+      $.ajax({
+        method: "POST",
+        url: "http://localhost:8080/tweets/",
+        data: serialiedData,
+        success: function (data) {
+          console.log("success!!", data);
+        },
+      });
+    }
+  });
+
   function renderTweets(tweets) {
     for (const tweet of tweets) {
       const $tweet = createTweetElement(tweet);
@@ -87,5 +58,11 @@ $(document).ready(function () {
     }
   }
 
-  renderTweets(data);
+  function loadTweets() {
+    $.get("/tweets", function (data) {
+      renderTweets(data);
+    });
+  }
+
+  loadTweets();
 });
